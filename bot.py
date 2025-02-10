a5bb03ddf7c9345e7e7d846b734a816223329b42
sensssoria <134649863+sensssoria@users.noreply.github.com>
2025-02-10 06:08:34 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from aiogram.types import (
     CallbackQuery
 )
 from aiogram.filters import Command
-from aiogram.dispatcher.filters import Text
+from aiogram.filters import Text
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.state import StatesGroup, State
